70f3ec25c717a1162ad08bbfb5bb0ac086a5e967
Fiadh Almohanna <fiadh.mohanna@gmail.com>
2019-03-13 21:37:30 +0300

    Complete TODOs

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,18 +1,20 @@
 package com.example.android.background.sync;
 
+import android.content.Context;
+import com.example.android.background.utilities.PreferenceUtilities;
+
 // TODO (1) Create a class called ReminderTasks
 public class ReminderTasks{
 // TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
-  public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+  public static final String ACTION_INCREMENT_WATER_COUNT = "value";
 // TODO (6) Create a public static void method called executeTask
-  public static void executeTask(Context context, String action){
 // TODO (7) Add a Context called context and String parameter called action to the parameter list
-
+    public static void executeTask(Context context, String action) {
 // TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
-    if(ACTION_INCREMENT_WATER_COUNT.equals(action)){
-      incrementWaterCount(context);
+        if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
+            incrementWaterCount(context);
+        }
     }
-  }
   
 // TODO (3) Create a private static void method called incrementWaterCount
 // TODO (4) Add a Context called context to the argument list
